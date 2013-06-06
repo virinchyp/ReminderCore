db0d8201794f32f0d86d4004b909c17123e6e90d
Anupam <anup.cancerian@gmail.com>
2013-06-06 00:24:35 -0400

    Generate id for objects

--- src/com/onesixty/seven/core/util/Util.java
+++ src/com/onesixty/seven/core/util/Util.java
@@ -1,6 +1,11 @@
 package com.onesixty.seven.core.util;
 
 public class Util {
-	// Only static methods go here.
-	//fuck you i will write other methods also
+	/**
+	 * 
+	 * @return
+	 */
+	public static long generateId() {
+		return System.currentTimeMillis();
+	}
 }
